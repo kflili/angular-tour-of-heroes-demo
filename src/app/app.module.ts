b77557228c959f752d6a3d65aea7fb827538b031
LiLi <kflili@gmail.com>
2017-04-23 00:40:55 -0400

    Add a dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroesComponent } from "./heroes.component";
 import { HeroService } from "./hero.service";
@@ -16,11 +17,21 @@ import { HeroService } from "./hero.service";
       {
         path: 'heroes',
         component: HeroesComponent
-      }
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
     ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
